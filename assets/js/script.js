fd2f743a6472ee5433ebad8a8b5804484407a962
harsh-android <harshche123@gmail.com>
2025-10-04 18:54:02 +0530

    Home Page 70%

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,28 @@ navLinks.forEach(link => {
         nav.classList.remove('active');
     });
 });
+
+  const counters = document.querySelectorAll(".stat-number");
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          counters.forEach(counter => {
+            const target = +counter.dataset.target;
+            const step = target / 100;
+            let count = 0;
+            const update = () => {
+              count += step;
+              if (count < target) {
+                counter.textContent = Math.floor(count);
+                requestAnimationFrame(update);
+              } else {
+                counter.textContent = target;
+              }
+            };
+            update();
+          });
+          observer.disconnect();
+        }
+      });
+    }, { threshold: 0.5 });
+    observer.observe(document.querySelector('.stats'));
